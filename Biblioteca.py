f215ebd150be5f659248e1f4ec0636122f20f5b9
florpatrone <patroneflorenciacarla@gmail.com>
2019-12-15 14:35:01 -0300

    Creada funcion 'camino_minimo'

--- Biblioteca.py
+++ Biblioteca.py
@@ -10,3 +10,23 @@ def imprimir_camino(camino):
 def listar_operaciones(operaciones):
     for op in operaciones:
         print(op)
+
+def camino_minimo(aeropuertos,origenes,destinos,pesado): #camino_mas camino_escalas
+    distancia_min = float('inf')
+    camino_min = []
+    algoritmo = dijkstra if pesado else bfs
+
+    for origen in origenes:
+        for destino in destinos:
+            camino_distancia = algoritmo(aeropuertos,origen,destino,None)[0]
+            
+            if pesado:
+                camino, distancia = camino_distancia
+            else:
+                camino, distancia = camino_distancia, len(camino_distancia)-1
+
+            if distancia < distancia_min:
+                distancia_min = distancia
+                camino_min = camino
+
+    imprimir_camino(camino_min)
